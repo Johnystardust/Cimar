7124546bd9f9cfe11f9eefb5a28d25a1831f528f
Tim van der Slik Laptop <timvdslik@gmail.com>
2015-11-09 09:28:36 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tim
+ * Date: 11/9/2015
+ * Time: 9:01 AM
+ */
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <!-- Scripts -->
+    <script src="includes/js/jquery-1.11.3.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+    <div class="container-fluid">
+
+        <div id="menu">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Over ons</a></li>
+                <li><a href="#">Expertises</a></li>
+                <li><a href="#">Toepassingen</a></li>
+                <li><a href="#">Nieuws</a></li>
+                <li><a href="#">Media</a></li>
+                <li><a href="#">Referenties</a></li>
+                <li><a href="#">Werken bij</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+
+    </div>
+
+
+
+
+<!--    --><?php //include_once('menu.php'); ?>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tim
+ * Date: 11/9/2015
+ * Time: 9:15 AM
+ */
