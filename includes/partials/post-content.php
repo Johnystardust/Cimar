596721b686ca236fa8d920f6dbe71ec2304dc3aa
Tim van der Slik Laptop <timvdslik@gmail.com>
2015-11-10 15:13:27 +0100

    Added Post single Added social share icons

---
+++ includes/partials/post-content.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tim
+ * Date: 11/10/2015
+ * Time: 2:41 PM
+ */
+?>
+
+<div class="col-md-9">
+
+    <div id="post-header">
+        <img src="includes/images/imagenews3.jpg" width="100%"/>
+    </div>
+
+    <div id="post-content">
+        <h4><strong>Capaciteitsuitwisseling</strong></h4>
+        <p>
+            Cimar Electronics is geen detacheringsbureau en kent geen buitendienst, maar wij zien capaciteitsuitwisseling wel als een toegevoegde
+            waarde in de nauwe samenwerking met onze partners en opdrachtgevers. Deze “collegiale uitleen” kan worden ingezet om medewerkers van
+            Cimar Electronics bij een opdrachtgever in te zetten en op te leiden; zodat hij of zij een product en/of werkwijze van de
+            opdrachtgever leert kennen. Heeft deze opdrachtgever in de toekomst een capaciteitsprobleem, dan kan (een deel van) de productie
+            moeiteloos worden overgedragen aan Cimar Electronics. Onze medewerkers zijn dan bekend met het product en de werkwijze, zodat er
+            voor de opdrachtgever geen verschil is tussen intern en extern geproduceerde exemplaren.
+            <br/><br/>
+            Door klantvoorraad aan te houden van (specifieke) onderdelen en fysieke ruimte te reserveren beschikken opdrachtgevers over
+            extra productiecapaciteit op het moment dat de markt hier om vraagt. Levert de klant bijvoorbeeld logistiek alles aan, dan kan er
+            desgewenst een grijpvoorraad van en vóór de klant worden opgeslagen waarbij Cimar enkel de arbeid levert. Uiteraard kunnen we ook zowel
+            materiaal als arbeid verzorgen.
+            <br/><br/>
+            Cimar Electronics ondersteunt diverse opdrachtgevers regelmatig bij het opvangen van (tijdelijke) capaciteitsproblemen. Dit kan door het
+            tijdelijk detacheren van onze professionals of door het (tijdelijk) overnemen van het gehele product of productielijn.
+            <br/><br/>
+            Wilt u meer weten over wat Cimar Electronics voor u kan betekenen op het gebeid van capaciteitsuitwisseling? Neem contact op via ons
+            contactformulier of bel met 0318- 550 545.
+        </p>
+
+        <hr class="line"/>
+
+        <div class="below-content">
+            <div class="back" style="float: left; height: 30px">
+                <a href="#" class="btn-cimar"><span><i class="icon icon-right-open"></i>Terug</span></a>
+            </div>
+
+            <div class="social-share">
+                <ul>
+                    <a href="#"><li><i class="icon icon-facebook"></i></li></a>
+                    <a href="#"><li><i class="icon icon-twitter"></i></li></a>
+                </ul>
+                <span><h4>Deel met uw netwerk</h4></span>
+            </div>
+        </div>
+
+
+
+
+
+
+    </div>
+
+
+</div>
---
+++ post.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tim
+ * Date: 11/10/2015
+ * Time: 2:39 PM
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="includes/stylesheet/style.css"/>
+
+    <!-- Fontello -->
+    <link rel="stylesheet" href="includes/fontello/fontello-embedded.css"/>
+
+</head>
+<body>
+
+<div class="container-fluid no-padding">
+
+    <?php include_once('includes/partials/menu.php'); ?>
+
+    <div id="right">
+
+        <div class="row no-margin">
+            <div id="page-title" class="col-md-12">
+                <h3>Capaciteitsuitwisseling</h3>
+                <hr/>
+                <hr/>
+            </div>
+        </div>
+
+        <div class="row no-margin">
+            <?php include_once('includes/partials/post-content.php'); ?>
+
+            <?php include_once('sidebar.php'); ?>
+
+        </div>
+
+        <?php include_once('footer.php'); ?>
+
+    </div><!-- end right -->
+
+</div><!-- end container -->
+
+</body>
+</html>
